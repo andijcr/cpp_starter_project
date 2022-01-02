9f0d8ae4d17b51a8ba17b4958f4944660c4f43bc
Andrea Barbadoro <andij.cr@gmail.com>
2022-01-02 22:31:02 +0100

    added cpm, doctest and fmt from github

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -1,9 +1,11 @@
+option(CPPStarterWarningsBeLenient "disable some of the warnings for a bit of peace" OFF)
+
 # from here:
 #
 # https://github.com/lefticus/cppbestpractices/blob/master/02-Use_the_Tools_Available.md
 
 function(set_project_warnings project_name)
-  option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" ON)
+  option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" OFF)
 
   set(MSVC_WARNINGS
       /W4 # Baseline reasonable warnings
@@ -37,18 +39,22 @@ function(set_project_warnings project_name)
       -Wshadow # warn the user if a variable declaration shadows one from a parent context
       -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
                          # catch hard to track down memory errors
-      -Wold-style-cast # warn for c-style casts
       -Wcast-align # warn for potential performance problem casts
       -Wunused # warn on anything being unused
-      -Woverloaded-virtual # warn if you overload (not override) a virtual function
+      $<$<COMPILE_LANGUAGE:CXX>:-Woverloaded-virtual> # warn if you overload (not override) a virtual function
       -Wpedantic # warn if non-standard C++ is used
-      -Wconversion # warn on type conversions that may lose data
-      -Wsign-conversion # warn on sign conversions
-      -Wnull-dereference # warn if a null dereference is detected
+	  $<$<VERSION_GREATER:${CMAKE_C_COMPILER_VERSION},5>:-Wnull-dereference> # warn if a null dereference is detected
       -Wdouble-promotion # warn if float is implicit promoted to double
       -Wformat=2 # warn on security issues around functions that format output (ie printf)
       -Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
   )
+ if(NOT CPPStarterWarningsBeLenient)
+   list(APPEND CLANG_WARNINGS
+     $<$<COMPILE_LANGUAGE:CXX>:-Wold-style-cast>   # warn for c-style casts
+     -Wconversion      # warn on type conversions that may lose data
+     -Wsign-conversion # warn on sign conversions
+   )
+ endif()
 
   if(WARNINGS_AS_ERRORS)
     set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
